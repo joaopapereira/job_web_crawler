dc31fa966854626a5c313b7eaf7acb13321a3c70
Joao Pereira <joaopapereira@gmail.com>
2016-05-24 17:48:48 -0400

    Added job and modified indeed to get the title and create jobs

---
+++ lib/crawler/searcher/job.rb
@@ -0,0 +1,22 @@
+module Crawler
+module Searcher
+    class Job
+        def initialize()
+            @title
+            @description
+        end
+        def title
+            @title
+        end
+        def title= title
+            @title = title
+        end
+        def description
+            @description
+        end
+        def description= description
+            @description = description 
+        end
+    end
+end
+end
--- lib/crawler/strategies/indeed.rb
+++ lib/crawler/strategies/indeed.rb
@@ -1,4 +1,5 @@
 require 'crawler/searcher/searcher'
+require 'crawler/searcher/job'
 require 'httparty'
 require 'nokogiri'
 require 'json'
@@ -10,27 +11,31 @@ class Indeed < Crawler::Searcher::Searcher
     page = HTTParty.get("http://rss.indeed.com/rss?q=#{term.gsub(/ /, '%20')}")
 
     parsed_page = Nokogiri::XML(page)
-    all_links = parsed_page.xpath("//item")
+    all_links = parsed_page.xpath("//item//link")
     to_visit = []
     
-    Pry.start(binding)
+    
     all_links.each do |link|
       to_visit << link.children.text
     end
 
 
     all_lines = []
+    all_jobs = []
 
     to_visit.each do |link|
       new_page = HTTParty.get(link)
-
+      job = Job.new
       parsed_page = Nokogiri::HTML(new_page)
       text = parsed_page.css('#job_summary').text
       if text.size > 1
+        job.title = parsef_page.css('.jobtitle').text
+        job.description = text
+        all_jobs << job
         all_lines << text
       end
     end
-
+    Pry.start(binding)
     add_result(term.to_sym, all_lines)
   end
 end
